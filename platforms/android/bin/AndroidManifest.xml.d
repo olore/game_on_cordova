4385b3764f148680cbc58162fc031ff17e7da425
Brian Olore <brian@olore.net>
2013-08-01 22:32:34 -0400

    clear out android & try again

--- platforms/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/brian/dev/game_on/game_on_cordova/platforms/android/bin/AndroidManifest.xml : \
-/Users/brian/dev/game_on/game_on_cordova/platforms/android/AndroidManifest.xml \
--- platforms/android/bin/HelloCordova-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Users/brian/dev/game_on/game_on_cordova/platforms/android/bin/HelloCordova-debug-unaligned.apk : \
-/Users/brian/dev/game_on/game_on_cordova/platforms/android/bin/HelloCordova.ap_ \
-/Users/brian/dev/game_on/game_on_cordova/platforms/android/bin/classes.dex \
-/Users/brian/dev/game_on/game_on_cordova/platforms/android/libs/cordova-3.0.0.jar \
