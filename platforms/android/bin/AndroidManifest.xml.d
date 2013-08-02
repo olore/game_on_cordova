8f13f8030bf567bbce815850ebf0b03937ffe100
Brian Olore <brian@olore.net>
2013-08-01 22:01:34 -0400

    initial checkin

---
+++ platforms/android/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/brian/dev/game_on/game_on_cordova/platforms/android/bin/AndroidManifest.xml : \
+/Users/brian/dev/game_on/game_on_cordova/platforms/android/AndroidManifest.xml \
---
+++ platforms/android/gen/R.java.d
@@ -0,0 +1,14 @@
+/Users/brian/dev/game_on/game_on_cordova/platforms/android/gen/io/cordova/hellocordova/R.java \
+ : /Users/brian/dev/game_on/game_on_cordova/platforms/android/res/drawable/icon.png \
+/Users/brian/dev/game_on/game_on_cordova/platforms/android/res/drawable-hdpi/ic_launcher.png \
+/Users/brian/dev/game_on/game_on_cordova/platforms/android/res/drawable-hdpi/icon.png \
+/Users/brian/dev/game_on/game_on_cordova/platforms/android/res/drawable-ldpi/ic_launcher.png \
+/Users/brian/dev/game_on/game_on_cordova/platforms/android/res/drawable-ldpi/icon.png \
+/Users/brian/dev/game_on/game_on_cordova/platforms/android/res/drawable-mdpi/ic_launcher.png \
+/Users/brian/dev/game_on/game_on_cordova/platforms/android/res/drawable-mdpi/icon.png \
+/Users/brian/dev/game_on/game_on_cordova/platforms/android/res/drawable-xhdpi/ic_launcher.png \
+/Users/brian/dev/game_on/game_on_cordova/platforms/android/res/drawable-xhdpi/icon.png \
+/Users/brian/dev/game_on/game_on_cordova/platforms/android/res/layout/main.xml \
+/Users/brian/dev/game_on/game_on_cordova/platforms/android/res/values/strings.xml \
+/Users/brian/dev/game_on/game_on_cordova/platforms/android/res/xml/config.xml \
+/Users/brian/dev/game_on/game_on_cordova/platforms/android/bin/AndroidManifest.xml \
